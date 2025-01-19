b01ba6b2d04efd3990ae2a6b8c95bc725cf136ff
miky-rola <mikyrola8@gmail.com>
2025-01-19 15:21:40 +0000

    updated the commands

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn print_help() {
     println!("  cargo run");
     println!("  cargo run -- help");
     println!("  cargo run -- clean-temp");
-    println!("  cargo run -- show-temp-files");
+    println!("  cargo run -- show-temp-files")
 }
 
 fn prompt_temp_file_age() -> Option<u64> {
